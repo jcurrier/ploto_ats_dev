dc06c81862c413ad0e8551ecf3e42a67dd1741cf
Jeff Currier <jeff.currier@gmail.com>
2014-06-01 00:00:40 -0700

    Integreated user authn with db checks.

--- app/com/ploto/controllers/Application.java
+++ app/com/ploto/controllers/Application.java
@@ -1,60 +1,82 @@
 package com.ploto.controllers;
 
 import com.ploto.services.Activity;
+import com.ploto.services.UserService;
+import com.ploto.services.store.StoreException;
+import com.ploto.settings.PlotoSettings;
+import com.ploto.util.PlotoContext;
 import play.*;
 import play.data.*;
 import play.data.Form;
 import play.mvc.*;
+
 import static play.data.Form.*;
 
 import views.html.helper.*;
 import com.ploto.views.html.*;
+
 import java.util.ArrayList;
 
 public class Application extends Controller {
+  private static PlotoContext mCtx = new PlotoContext();
 
-    public static Result login() {
-        return ok(login.render(form(Login.class)));
+  public static Result login() {
+    return ok(login.render(form(Login.class)));
+  }
+
+  @Security.Authenticated(Secured.class)
+  public static Result index() {
+    ArrayList<Activity> activities = new ArrayList<Activity>();
+    activities.add(new Activity("activity 1", "user", "Sunday 7:00PM"));
+    activities.add(new Activity("activity 2", "user 2", "Sunday 9:00PM"));
+    return ok(main.render("Your new application is ready.", activities));
+  }
+
+  @Security.Authenticated(Secured.class)
+  public static Result logout() {
+    session().clear();
+    flash("success", "You've been logged out");
+    return redirect(
+        com.ploto.controllers.routes.Application.login()
+    );
+  }
+
+  public static Result authenticate() {
+    System.out.println("In App::authenticate");
+    Form<Login> loginForm = form(Login.class).bindFromRequest();
+    System.out.println("user = " + loginForm.data().get("email") + " pw = " + loginForm.data().get("password"));
+
+    if (loginForm.hasErrors()) {
+      return badRequest(com.ploto.views.html.login.render(loginForm));
+    } else {
+      session("email", loginForm.get().email);
+      return redirect(
+          com.ploto.controllers.routes.Application.index()
+      );
     }
+  }
 
-    @Security.Authenticated(Secured.class)
-    public static Result index() {
-        ArrayList<Activity> activities = new ArrayList<Activity>();
-        activities.add(new Activity("activity 1", "user", "Sunday 7:00PM"));
-        activities.add(new Activity("activity 2", "user 2", "Sunday 9:00PM"));
-        return ok(main.render("Your new application is ready.", activities));
-    }
+  public static class Login {
+    private static PlotoContext mCtx = new PlotoContext();
 
-    @Security.Authenticated(Secured.class)
-    public static Result logout() {
-        session().clear();
-        flash("success", "You've been logged out");
-        return redirect(
-                com.ploto.controllers.routes.Application.login()
-        );
-    }
+    public String email;
+    public String password;
 
-    public static Result authenticate() {
-        System.out.println("In App::authenticate");
-        Form<Login> loginForm = form(Login.class).bindFromRequest();
-        System.out.println("user = " + loginForm.data().get("email") + " pw = " + loginForm.data().get("password"));
-        if(loginForm.hasErrors()) {
-            return badRequest(com.ploto.views.html.login.render(loginForm));
-        } else {
-            session("email", loginForm.get().email);
-            return redirect(
-                    com.ploto.controllers.routes.Application.index()
-            );
+    public String validate() {
+      String verdict = null;
+
+      try {
+        UserService userSvc = mCtx.getInjector().getInstance(UserService.class);
+
+        if(!userSvc.authenicateUser(email, password)) {
+          verdict = "Invalid credentials";
         }
-    }
+      } catch (Exception ex) {
+        verdict = "internal error";
+      }
 
-    public static class Login {
-        public String email;
-        public String password;
-
-        public String validate() {
-            System.out.println("In Login::validate");
-            return null;
-        }
+      System.out.println("In Login::validate");
+      return verdict;
     }
+  }
 }
--- app/com/ploto/settings/PlotoSettings.java
+++ app/com/ploto/settings/PlotoSettings.java
@@ -1,5 +1,11 @@
 package com.ploto.settings;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.ploto.services.JobService;
+import com.ploto.services.store.JobServiceSqlStore;
+import com.ploto.services.store.JobServiceStore;
 import com.ploto.util.PlotoContext;
 import play.Application;
 import play.GlobalSettings;
@@ -10,30 +16,20 @@ import play.GlobalSettings;
 public class PlotoSettings extends GlobalSettings {
   private PlotoContext mCtx = null;
 
-    /*
-    private static Injector mInjector = null;
+  /*
+  private static Injector mInjector = null;
 
-    public static Injector getInjector() {
-        return mInjector;
-    }
-    (*/
-
+  public static Injector getInjector() {
+    return mInjector;
+  }
+  */
 
   @Override
   public void onStart(Application application) {
     super.onStart(application);
+    /*
     mCtx = new PlotoContext();
-        /*
-            mInjector = Guice.createInjector(new AbstractModule() {
-
-                @Override
-                protected void configure() {
-                    bind(JobServiceStore.class).to(JobServiceSqlStore.class);
-                    bind(JobService.class);
-
-                    System.out.println("Injections bound.");
-                }
-            });
-            */
+    mCtx.createProdBindings();
+    */
   }
 }
